68a5e7f77bb54f9c83e344b3c4f7b9d9dd5c6cff
Sam Rowe <sgrowe@live.co.uk>
2023-05-07 20:00:34 +0100

    Update prettier and tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,29 @@ module.exports = {
       listStyleType: {
         dash: "'– '",
       },
+      animation: {
+        sunrise: 'sunrise 3s ease 0.5s both',
+        'face-bob-up-and-down':
+          'face-bob-up-and-down 4s ease-in-out 3.5s infinite both',
+        'arrow-bob-right-and-left':
+          'arrow-bob-right-and-left 2.3s ease-in-out 5s infinite both',
+        'arrow-bob-right-and-left-fast':
+          'arrow-bob-right-and-left 1s ease-in-out 0.2s infinite both',
+      },
+      keyframes: {
+        sunrise: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-50%)' },
+        },
+        'face-bob-up-and-down': {
+          '0%, 100%': { transform: 'translateY(-8px)' },
+          '50%': { transform: 'translateY(8px)' },
+        },
+        'arrow-bob-right-and-left': {
+          '0%, 20%, 40%': { transform: 'translateX(0)' },
+          '10%, 30%': { transform: 'translateX(10px)' },
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
