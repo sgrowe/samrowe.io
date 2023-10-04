61bb253357774145cbd3bf4764015c639ade249d
Sam Rowe <sgrowe@live.co.uk>
2023-10-04 17:01:07 +0100

    Fix issues after updating

--- src/components/tech-logos/DynamoDBLogo.tsx
+++ src/components/tech-logos/DynamoDBLogo.tsx
@@ -2,6 +2,6 @@ import dynamoDB from './dynamoDB.png'
 
 export const DynamoDBLogo = () => (
   <div className="flex h-full flex-col justify-center p-1">
-    <img src={dynamoDB} alt="DynamoDB logo" />
+    <img src={dynamoDB.src} alt="DynamoDB logo" />
   </div>
 )
